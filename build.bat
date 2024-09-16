3578c6999a6cec54b23d616a3b6046baf55d01ad
Brochu <alexbrochu1@gmail.com>
2024-09-15 23:54:30 -0400

    [SETUP] Scripts to help build and clean executable

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo off
 
-cl /Od /Ob1 /DBUILD_DEBUG=1 /I.\include\ /nologo /FC /Z7 .\src\main.c /link /MANIFEST:EMBED /INCREMENTAL:NO /out:GITS-Viewer.exe
+cl /Od /Ob1 /DBUILD_DEBUG=1 /I.\include\ /I.\include\assimp /nologo /FC /Z7 /MD .\src\main.c ^
+/link /MANIFEST:EMBED /INCREMENTAL:NO /LIBPATH:libs\ /NODEFAULTLIB:MSVCRTD ^
+assimp-vc143-mt.lib raylib.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib user32.lib shell32.lib ^
+/out:GITS-Viewer.exe
+
 del main.obj
---
+++ clean.bat
@@ -0,0 +1,3 @@
+@echo off
+del GITS-Viewer.exe
+del GITS-Viewer.pdb
