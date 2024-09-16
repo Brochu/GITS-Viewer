a5a3a44e171a9291d7005617d7cf21affb004daf
Brochu <alexbrochu1@gmail.com>
2024-09-15 22:24:37 -0400

    [INIT] Start main C entrypoint

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int main(int argc, char **argv) {
+    printf("[GITS-V] Program starting\n");
+
+    printf("Runrunrunrurn\n");
+
+    printf("[GITS-V] Program exiting\n");
+    return 0;
+}
