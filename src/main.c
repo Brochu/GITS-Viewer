85f675147b53a2bfa5bbb2a8710a166af5458aa0
Brochu <alexbrochu1@gmail.com>
2024-09-15 23:54:45 -0400

    [DEV] Testing linking of assimp and raylib libs

--- src/main.c
+++ src/main.c
@@ -1,10 +1,43 @@
 #include "stdio.h"
 
-int main(int argc, char **argv) {
-    printf("[GITS-V] Program starting\n");
+#include "cimport.h"
+#include "scene.h"
+#include "postprocess.h"
 
-    printf("Runrunrunrurn\n");
+#include "raylib.h"
 
-    printf("[GITS-V] Program exiting\n");
+int main(void) {
+    SetTraceLogLevel(LOG_DEBUG);
+    TraceLog(LOG_DEBUG, "[GITS-V] Program starting");
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Setting up assimp logging");
+    struct aiLogStream stream;
+    stream = aiGetPredefinedLogStream(aiDefaultLogStream_STDOUT, NULL);
+    aiAttachLogStream(&stream);
+
+    const char *path = ".\\data\\Slingshot\\slingshot.obj";
+    const struct aiScene *scene = aiImportFile(path, aiProcessPreset_TargetRealtime_Quality);
+    struct aiMesh *m = scene->mMeshes[0];
+    struct aiString name = m->mName;
+    size_t numFaces = m->mNumFaces;
+    TraceLog(LOG_DEBUG, "[GITS-V] Slingshot mesh: [%zu] '%s'", numFaces, name.data);
+    aiReleaseImport(scene);
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Preparing Raylib Window");
+    InitWindow(800, 600, "GITS-Viewer A0");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Clearing Raylib");
+    CloseWindow();
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Clearing assimp logging");
+    aiDetachAllLogStreams();
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Program exiting");
     return 0;
 }
