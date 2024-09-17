256861489ff3591d1f19ab969e1a3528cde6666b
Brochu <alexbrochu1@gmail.com>
2024-09-16 23:19:07 -0400

    [DEV] Render first model with pure raylib

--- src/main.c
+++ src/main.c
@@ -1,42 +1,43 @@
-#include "stdio.h"
-
-#include "cimport.h"
-#include "scene.h"
-#include "postprocess.h"
-
 #include "raylib.h"
+#include "raymath.h"
+
+Camera3D cam;
 
 int main(void) {
     SetTraceLogLevel(LOG_DEBUG);
     TraceLog(LOG_DEBUG, "[GITS-V] Program starting");
 
-    TraceLog(LOG_DEBUG, "[GITS-V] Setting up assimp logging");
-    struct aiLogStream stream;
-    stream = aiGetPredefinedLogStream(aiDefaultLogStream_STDOUT, NULL);
-    aiAttachLogStream(&stream);
-
-    const char *path = ".\\data\\Slingshot\\slingshot.obj";
-    const struct aiScene *scene = aiImportFile(path, aiProcessPreset_TargetRealtime_Quality);
-    struct aiMesh *m = scene->mMeshes[0];
-    struct aiString name = m->mName;
-    size_t numFaces = m->mNumFaces;
-    TraceLog(LOG_DEBUG, "[GITS-V] Slingshot mesh: [%zu] '%s'", numFaces, name.data);
-    aiReleaseImport(scene);
-
     TraceLog(LOG_DEBUG, "[GITS-V] Preparing Raylib Window");
     InitWindow(800, 600, "GITS-Viewer A0");
+    DisableCursor();
+
+    cam = (Camera3D) {
+        .position = (Vector3) { 0.f, 3.f, -10.f },
+        .target = (Vector3) { 0.f, 0.f, 0.f },
+        .up = (Vector3) { 0.f, 1.f, 0.f },
+        .fovy = 45.f,
+        .projection = CAMERA_PERSPECTIVE,
+    };
+
+    TraceLog(LOG_DEBUG, "[GITS-V] Loading test model");
+    const char *path = ".\\data\\Toaster\\Toaster.obj";
+    Model model = LoadModel(path);
 
     while (!WindowShouldClose()) {
+        UpdateCamera(&cam, CAMERA_THIRD_PERSON);
+
         BeginDrawing();
         ClearBackground(BLACK);
+
+        BeginMode3D(cam);
+        DrawModel(model, Vector3Zero(), 2.0f, WHITE);
+        EndMode3D();
+
         EndDrawing();
     }
 
     TraceLog(LOG_DEBUG, "[GITS-V] Clearing Raylib");
     CloseWindow();
-
-    TraceLog(LOG_DEBUG, "[GITS-V] Clearing assimp logging");
-    aiDetachAllLogStreams();
 
     TraceLog(LOG_DEBUG, "[GITS-V] Program exiting");
     return 0;
