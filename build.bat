c5c44d505ba0a01200bace7468a75c357c5d60ef
Brochu <alexbrochu1@gmail.com>
2024-09-16 23:18:54 -0400

    [SETUP] Maybe assimp won't be necessary? Keep it in files for now, just dont link

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 cl /Od /Ob1 /DBUILD_DEBUG=1 /I.\include\ /I.\include\assimp /nologo /FC /Z7 /MD .\src\main.c ^
 /link /MANIFEST:EMBED /INCREMENTAL:NO /LIBPATH:libs\ /NODEFAULTLIB:MSVCRTD ^
-assimp-vc143-mt.lib raylib.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib user32.lib shell32.lib ^
+raylib.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib user32.lib shell32.lib ^
 /out:GITS-Viewer.exe
 
 del main.obj
