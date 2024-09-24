6f5347c4a76b0bb206477c28b78da8ca92384972
Brochu <alexbrochu1@gmail.com>
2024-09-24 19:48:30 -0400

    [DEV] WIP: Start work on changing which model to load and show, need to debug a couple of them

--- src/main.c
+++ src/main.c
@@ -8,6 +8,44 @@
 
 Camera3D cam;
 
+typedef enum {
+    // MAIN WEAPONS
+    SLINGSHOT,
+    WRENCH,
+    TETHER,
+    MINELASER,
+    RIPPER,
+    TOASTER,
+    SPEW,
+    SCATTER,
+    RIVET,
+    BAZOOKA,
+
+    // GRENADES
+    CORINGG,
+    RECRUITG,
+    CLEANERG,
+} weapon_t;
+
+static const char *paths[] = {
+    ".\\data\\Slingshot\\slingshot.obj",
+    ".\\data\\Wrench\\wrench.obj",
+    ".\\data\\Tether\\ControlTether.obj",
+    ".\\data\\MiningLaser1\\MiningLaserLevel1.obj",
+    ".\\data\\Ripper\\Ripper.obj",
+    ".\\data\\Toaster\\Toaster.obj",
+    ".\\data\\Spew1\\Spew1.obj",
+    ".\\data\\Scattergun\\ScattergunLV1.obj",
+    ".\\data\\Rivetgun2\\RivetgunLV1.obj",
+    ".\\data\\RocketLauncher1\\Rocketlauncher.obj",
+
+    ".\\data\\CoringCharge\\CoringCharge.obj",
+    ".\\data\\RecruitGrenade\\Recruiter.obj",
+    ".\\data\\Cleaner\\Cleaner.obj",
+};
+
+weapon_t shownWeapon = (weapon_t)0;
+
 int main(void) {
     SetTraceLogLevel(LOG_DEBUG);
     TraceLog(LOG_DEBUG, "[GITS-V] Program starting");
@@ -27,10 +65,16 @@ int main(void) {
     };
 
     TraceLog(LOG_DEBUG, "[GITS-V] Loading test model");
-    const char *path = ".\\data\\Tether\\ControlTether.obj";
-    Model model = LoadModel(path);
+    Model model = LoadModel(paths[shownWeapon]);
 
     while (!WindowShouldClose()) {
+        //TODO: Reload model on the fly
+        if (IsKeyPressed(KEY_W)) {
+            shownWeapon++;
+        } else if (IsKeyPressed(KEY_S)) {
+            shownWeapon--;
+        }
+
         UpdateCamera(&cam, CAMERA_ORBITAL);
 
         BeginDrawing();
